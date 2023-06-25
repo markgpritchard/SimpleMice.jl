9565bd6a8517ed7e68d321aa6934a2615563e4cb
markgpritchard <62439333+markgpritchard@users.noreply.github.com>
2023-06-25 10:48:24 +0100

    Update extras.jl

--- src/extras.jl
+++ src/extras.jl
@@ -8,5 +8,5 @@ Based on code shared at
     https://discourse.julialang.org/t/disable-sentinelarrays-for-csv-read/54843/3
 """
 function desentinelize!(df::DataFrame)
-    for (i, col) in enumerate(eachcol(df)) df[!, i] = collect(col) end
+    for (i, col) ∈ enumerate(eachcol(df)) df[!, i] = collect(col) end
 end
