2e4a7b503126239213ba46552feb24603342c19d
markgpritchard <62439333+markgpritchard@users.noreply.github.com>
2025-02-22 11:00:39 +0000

    re-write

--- src/SimpleMice.jl
+++ src/SimpleMice.jl
@@ -1,32 +1,222 @@
 
 module SimpleMice
 
-using DataFrames, Distributions, GLM, Random, StatsBase
-using HypothesisTests: confint, OneSampleTTest, pvalue
-import Base: eltype
-import DataAPI: describe
-import StatsBase: mean, std, summarystats, var
-import GLM: fit, glm, lm
+using AutoHashEquals, DataFrames, GLM, Random, StaticArrays, StatsBase
+import Base: ^, /, *, +, -, sin, cos, tan, log, exp, getindex, isapprox, iterate, setindex!
 
-include("types.jl")
-include("constants.jl")
-include("mice.jl")
-include("classifyvariables.jl")
-include("basefunctions.jl")
-include("dataapifunctions.jl")
-include("statsfunctions.jl")
-include("glmfunctions.jl")
-include("extras.jl")
-include("testdataset.jl")
+export MiceValue
+export ^, /, *, +, -, sin, cos, tan, log, exp, getindex, isapprox, iterate, setindex!
+export initializemice, initializemice!, micevalues, nonemissing, updatemicevalues!
 
-export mice
-export describe, eltype, getvalues,
-    betweenimputationvar, rubinsmean, rubinssterror, rubinsvar, 
-    withinimputationsterrorsquared, withinimputationvar,
-    componentmeans, componentstats, componentvars,
-    mean, meanstats, std, summarystats, var,
-    fit, glm, lm,
-    desentinelize!, oneach,
-    mcar, mcar!
+abstract type AbstractMiceValue end 
+
+@auto_hash_equals struct MiceValue{N, T} <: AbstractMiceValue 
+    x       :: MVector{N, T} 
+end
+
+function MiceValue(v::AbstractVector) 
+    N = length(v)
+    return MiceValue(N, v) 
+end
+
+function MiceValue(N::Integer, v::AbstractVector) 
+    return MiceValue(MVector{N}(v)) 
+end
+
+getindex(a::AbstractMiceValue, i) = a.x[i]
+iterate(a::AbstractMiceValue) = iterate(a.x)
+iterate(a::AbstractMiceValue, i) = iterate(a.x, i)
+
+function isapprox(a::AbstractMiceValue, b::AbstractMiceValue; kwargs...)
+    return isapprox(a.x, b.x; kwargs...)
+end
+
+setindex!(a::AbstractMiceValue, v, i) = setindex!(a.x, v, i)
+
+for f ∈ [ :^, :/, :*, :+, :- ]
+    eval(quote
+        function $f(a::AbstractMiceValue, b::AbstractMiceValue)
+            v = @. $f(a.x, b.x)
+            return MiceValue(v)
+        end
+        function $f(a::AbstractMiceValue, b::Number)
+            v = @. $f(a.x, b)
+            return MiceValue(v)
+        end
+        function $f(a::Number, b::AbstractMiceValue)
+            v = @. $f(a, b.x)
+            return MiceValue(v)
+        end
+    end)
+end
+
+for f ∈ [ :sin, :cos, :tan, :log, :exp ]
+    eval(quote
+        function $f(a::AbstractMiceValue)
+            v = @. $f(a.x)
+            return MiceValue(v)
+        end
+    end)
+end
+
+nonemissing(x::AbstractVector) = sum([ ismissing(xi) for xi ∈ x ]) == 0
+
+function initializemice(N::Integer, x::AbstractVector)
+    return initializemice(Random.default_rng(), N, x)
+end
+
+function initializemice(rng::Random.AbstractRNG, N::Integer, x::AbstractVector)
+    return initializemice(rng, Float64, N, x)
+end
+
+function initializemice(T::DataType, N::Integer, x::AbstractVector)
+    return initializemice(Random.default_rng(), T, N, x)
+end
+
+function initializemice(rng::Random.AbstractRNG, T::DataType, N::Integer, x::AbstractVector)
+    if nonemissing(x) 
+        return x 
+    end
+    nmvector = collect(skipmissing(x))
+    @assert length(nmvector) >= 1 "Must have at least 1 non-missing value"
+    Z = typeof(nmvector[1])
+    return Union{Z, MiceValue{N, T}}[
+        ismissing(xi) ? 
+            MiceValue(N, _samplemice(rng, T, N, nmvector)) :
+            xi 
+        for xi ∈ x 
+    ]
+end
+
+_samplemice(rng, T, N, nmvector) = [ T(sample(rng, nmvector)) for _ ∈ 1:N ]
+
+function initializemice!(N::Integer, df::DataFrame)
+    initializemice!(Random.default_rng(), N, df)
+end
+
+function initializemice!(N::Integer, df::DataFrame, i)
+    initializemice!(Random.default_rng(), N, df, i)
+end
+
+function initializemice!(rng::Random.AbstractRNG, N::Integer, df::DataFrame)
+    for (i, colname) ∈ enumerate(names(df))
+        _initializemice!(rng, N, df, colname, i)
+    end
+end
+
+function initializemice!(rng::Random.AbstractRNG, N::Integer, df::DataFrame, i::AbstractVector)
+    for ix ∈ i 
+        initializemice!(rng, N, df, ix)
+    end
+end
+
+function initializemice!(
+    rng::Random.AbstractRNG, N::Integer, df::DataFrame, colname::AbstractString
+)
+    if nonemissing(getproperty(df, colname)) 
+        return nothing 
+    end 
+    i = findfirst(x -> x == colname, names(df)) 
+    __initializemice!(rng, N, df, colname, i)
+end
+
+function initializemice!(rng::Random.AbstractRNG, N::Integer, df::DataFrame, colname::Symbol)
+    if nonemissing(getproperty(df, colname)) 
+        return nothing 
+    end 
+    i = findfirst(x -> x == String(colname), names(df)) 
+    __initializemice!(rng, N, df, colname, i)
+end
+
+function initializemice!(rng::Random.AbstractRNG, N::Integer, df::DataFrame, i::Integer)
+    colname = names(df)[i]
+    _initializemice!(rng, N, df, colname, i)
+end
+
+function _initializemice!(rng, N, df, colname, i)
+    if nonemissing(getproperty(df, colname)) 
+        return nothing 
+    end 
+    __initializemice!(rng, N, df, colname, i)
+end
+
+function __initializemice!(rng, N, df, colname, i)
+    newvec = initializemice(rng, N, getproperty(df, colname))
+    select!(df, Not(colname))
+    insertcols!(df, i, colname => newvec)
+end
+
+micevalues(x::AbstractVector, i) = [ _micevalue(xi, i) for xi ∈ x ]
+
+function micevalues(df::DataFrame, i)
+    namevec = names(df)
+    return micevalues(df, namevec, i)
+end
+
+function micevalues(df::DataFrame, columnindexes::AbstractVector, i::Integer)
+    M = zeros(Float64, size(df, 1), length(columnindexes))
+    for (j, index) ∈ enumerate(columnindexes)
+        M[:, j] .= micevalues(df, index, i)
+    end
+    return M
+end
+
+function micevalues(df::DataFrame, columnindex::Integer, i::Integer)
+    colname = names(df)[columnindex]
+    return micevalues(df, colname, i)
+end
+
+function micevalues(
+    df::DataFrame, colname::S, i::Integer
+) where S <: Union{<:AbstractString, Symbol}
+    return micevalues(getproperty(df, colname), i)
+end
+
+_micevalue(a::AbstractMiceValue, i) = getindex(a, i)
+_micevalue(a::Number, ::Integer) = a
+
+function _linearfit(df, x, y, i)
+    xmat = micevalues(df, x, i)
+    yvec = micevalues(df, y, i)
+    return fit(LinearModel, xmat, yvec)
+end
+
+function updatemicevalues!(
+    df, y::S, x::T, N
+) where {S <: Union{<:AbstractString, Symbol}, T <: Union{S, <:AbstractVector{S}}} 
+    for i ∈ 1:N 
+        _updatemicevalues!(df, y, x, i)
+    end 
+end
+
+function updatemicevalues!(
+    df, y::Integer, x::T, N
+) where T <: Union{<:Integer, <:AbstractVector{<:Integer}}
+    ycolname = names(df)[y]
+    xcolnames = names(df)[x]
+    updatemicevalues!(df, ycolname, xcolnames, N)
+end
+
+function _updatemicevalues!(df, y, x, i)
+    reg = _linearfit(df, x, y, i) 
+    predictions = predict(reg)
+    __updatemicevalues!(df, y, predictions, i)
+end
+
+function __updatemicevalues!(df, y, predictions, i) 
+    for j ∈ axes(df, 1)
+        __updatemicevalues!(df, y, predictions, i, j)
+    end
+end
+
+function __updatemicevalues!(df, y::S, predictions, i, j) where S <: Union{<:AbstractString, Symbol} 
+    ___updatemicevalues!(df, y, getproperty(df, y)[j], predictions, i, j)
+end
+
+___updatemicevalues!(::Any, ::Any, ::Number, ::Any, ::Any, ::Any) = nothing
+
+function ___updatemicevalues!(df, y::S, existingvalue::AbstractMiceValue, predictions, i, j) where S <: Union{<:AbstractString, Symbol} 
+    getproperty(df, y)[j][i] = predictions[j]
+end
 
 end # module SimpleMice
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,101 +1,305 @@
 
 using SimpleMice
 using Test
-using DataFrames, Distributions, Random
-
-Random.seed!(1729)
-const NOMISSINGDATA = SimpleMice.testdataset()
-const MCAR1 = mcar(
-    NOMISSINGDATA, 
-    [ :Ages, :Sexes, :Vara, :Varb, :Varc, :Vard, :Vare, :Varf, :Varg ],
-    .01
-)
-const MCAR50 = mcar(
-    NOMISSINGDATA, 
-    [ :Ages, :Sexes, :Vara, :Varb, :Varc, :Vard, :Vare, :Varf, :Varg ],
-    .5
-)
+using DataFrames
+using StableRNGs
 
 @testset "SimpleMice.jl" begin
-    @testset "MCAR function" begin 
-        @test 3 <= minimum(describe(MCAR1).nmissing[1:8]) <= 10
-        @test 10 <= maximum(describe(MCAR1).nmissing[1:8]) <= 30
-        @test 200 <= minimum(describe(MCAR50).nmissing[1:8]) <= 500
-        @test 500 <= maximum(describe(MCAR50).nmissing[1:8]) <= 800
-    end # @testset "MCAR function"
-    @testset "Imputation tests" begin
-        @testset "List of symbols" begin 
-            @test SimpleMice.symbollist([ :a, :b, :c ]) == [ :a, :b, :c ] 
-            @test SimpleMice.symbollist([ "a", "b", "c" ]) == [ :a, :b, :c ] 
-            @test SimpleMice.symbollist(nothing) == nothing
-            @test SimpleMice.symbollist(Float64[]) == Symbol[]
-            @test_throws AssertionError SimpleMice.symbollist([ 2 ])
-        end # @testset "List of symbols"
-        
-        @testset "Classification of variables" begin
-            @testset "None missing" begin 
-                vars = Symbol.(names(NOMISSINGDATA))
-                bv, cv, niv = SimpleMice.classifyvars!(vars, nothing, nothing, nothing, NOMISSINGDATA; 
-                    printdropped = false)
-                @test bv == Symbol[]
-                @test cv == Symbol[]
-                @test niv == [ :Ages, :Sexes, :Vara, :Varb, :Varc, :Vard, :Vare, :Varf, :Varg, :Outcome ]
-                @test vars == Symbol[]
-            end # @testset "None missing"
-            @testset "Auto - some missing" for df ∈ [ MCAR1, MCAR50 ]
-                vars = Symbol.(names(df))
-                bv, cv, niv = SimpleMice.classifyvars!(vars, nothing, nothing, nothing, df; 
-                    printdropped = false)
-                @test bv == [ :Sexes, :Vara, :Varb, :Varc, :Varf, :Varg ]
-                @test cv == [ :Ages, :Vard, :Vare ]
-                @test niv == [ :Outcome ]
-                @test vars == Symbol[]
-            end # @testset "Auto - some missing" for df ∈ [ MCAR1, MCAR50 ]
-            @testset "Manual binary" for df ∈ [ MCAR1, MCAR50 ]
-                vars = Symbol.(names(df))
-                bv, cv, niv = SimpleMice.classifyvars!(vars, [ :Sexes ], nothing, nothing, df; 
-                    printdropped = false)
-                @test bv == [ :Sexes ] 
-                @test cv == [ :Ages, :Vara, :Varb, :Varc, :Vard, :Vare, :Varg ]
-                @test niv == [ :Outcome ]
-                @test vars == [ :Varf ]
-            end # @testset "Manual binary" for df ∈ [ MCAR1, MCAR50 ]
-        end # @testset "Classification of variables"
-        
-        @testset "No change if no missing values" begin
-            micedata = mice(NOMISSINGDATA, [ :Ages, :Sexes, :Vara, :Varb, :Varc, :Vard, :Vare, :Varf, :Varg ]; 
-                n = 2, verbose = false)
-            @test isa(micedata, SimpleMice.ImputedDataFrame)
-            @test micedata.originaldf == NOMISSINGDATA
-            @test micedata.numberimputed == 2
-            @testset for i ∈ 1:2 @test micedata.imputeddfs[i] == NOMISSINGDATA end 
-        end # @testset "No change if no missing values"
-        
-        @testset "Similar if only 1% missing" begin
-            micedata = mice(MCAR1, [ :Ages, :Sexes, :Vara, :Varb, :Varc, :Vard, :Vare, :Varf, :Varg ]; 
-                m = 1000, n = 10, verbose = false)
-            @test isa(micedata, SimpleMice.ImputedDataFrame)
-            @test skipmissing(micedata.originaldf) == skipmissing(MCAR1)
-            @test micedata.numberimputed == 10
-            @testset for v ∈ [ :Ages, :Vara, :Varb, :Vard, :Vare, :Varg ]
-                # I'm not sure how reasonable this test is but currently the package passes it
-                # Update: :Varc now fails the test. This variable is uncorrelated 
-                # with other variables -- currently just removing from the test. 
-                nomissingmean = mean(getproperty(NOMISSINGDATA, v))
-                imputedmean = mean(getvalues(micedata, v))
-                @test .985 * nomissingmean <= imputedmean <= 1.015 * nomissingmean 
-                nomissingvar = var(getproperty(NOMISSINGDATA, v))
-                imputedvar = var(getvalues(micedata, v))
-                @test .985 * nomissingvar <= imputedvar <= 1.015 * nomissingvar 
-            end # @testset for v ∈ [ :Ages, :Vara, :Varb, :Varc, :Vard, :Varf ] 
-        end #  @testset "Similar if only 1% missing"
-        
-        @testset "Function works if 50% missing" begin
-            micedata = mice(MCAR50, [ :Ages, :Sexes, :Vara, :Varb, :Varc, :Vard, :Vare, :Varf, :Varg ]; 
-                n = 5, verbose = false)
-            @test isa(micedata, SimpleMice.ImputedDataFrame)
-            @test skipmissing(micedata.originaldf) == skipmissing(MCAR50)
-            @test micedata.numberimputed == 5 
-        end # @testset "Function works if 50% missing" 
-    end # @testset "Imputation tests"
-end # @testset "SimpleMice.jl"
+@testset "Equality and differences in MiceValues" begin
+    @test MiceValue(2, [ 0, 0 ]) == MiceValue(2, [ 0, 0 ])
+    @test MiceValue(2, [ 0, 0 ]) == MiceValue(2, [ 0.0, 0.0 ])
+    @test MiceValue(2, [ 0, 0 ]) != MiceValue(2, [ 0, 1 ])
+    @test MiceValue(2, [ 0, 0 ]) != MiceValue(3, [ 0, 0, 0 ])
+    @test MiceValue(2, [ 1, 1 ]) ≈ MiceValue(2, [ 1.0, 1.0 ])
+    @test !isapprox(MiceValue(2, [ 1, 1 ]), MiceValue(2, [ 1.0, 2.0 ]))
+    @test MiceValue(2, [ 1, 1 ]) ≈ MiceValue(2, [ 1.0, 1 + 1e-20 ])
+    @test isapprox(MiceValue(2, [ 0.1, 0.1 ]), MiceValue(2, [ 0.1, 0.15 ]); atol=0.05)
+    @test isapprox(MiceValue(2, [ 0.1, 0.1 ]), MiceValue(2, [ 0.1, 0.15 ]); rtol=0.34)
+end
+@testset "Convert missing values into `Mice` values" begin
+    # recognise vectors with no missing values 
+    @test nonemissing(ones(2))
+    # recognise vectors with at least one missing value 
+    @test !nonemissing([ 1, missing ])
+    # does not modify a vector with nothing missing
+    @test initializemice(5, ones(1)) == ones(1)    
+    # if nothing missing, returns same type as is provided 
+    @test initializemice(5, ones(Int, 1)) isa Vector{Int64} 
+    # values of missing data populated with other values from the vector 
+    @test initializemice(5, [ 1, missing ]) == [ 1, MiceValue(5, ones(Int, 5)) ]
+    # provides correct number of imputed values 
+    @test initializemice(4, [ 1, missing ]) == [ 1, MiceValue(4, ones(Int, 4)) ]
+    # imputed values of correct type 
+    @test initializemice(5, [ Float64(1), missing ]) == [ 1, MiceValue(5, ones(Float64, 5)) ]
+    # non-missing values are not changed 
+    @test initializemice(5, [ 2, missing ]) == [ 2, MiceValue(5, 2 .* ones(Int, 5)) ]
+    # converted in whichever order 
+    @test initializemice(5, [ missing, 1 ]) == [ MiceValue(5, ones(Int, 5)), 1 ]
+    @testset "Test sampled values" begin
+        rng = StableRNG(1)
+        a = initializemice(rng, 10, [ 0, 1, 2, missing ])
+        @test minimum(a[4]) == 0
+        @test maximum(a[4]) == 2
+    end
+    @testset "same rng gives same results" begin
+        rng_a = StableRNG(1)
+        a = initializemice(rng_a, 10, [ 0, 1, 2, missing ])
+        rng_b = StableRNG(1)
+        b = initializemice(rng_b, 10, [ 0, 1, 2, missing ])        
+        @test a == b
+    end
+    @testset "different rng gives different results" begin
+        rng_a = StableRNG(1)
+        a = initializemice(rng_a, 10, [ 0, 1, 2, missing ])
+        rng_b = StableRNG(2)
+        b = initializemice(rng_b, 10, [ 0, 1, 2, missing ])        
+        @test a != b
+    end
+    # assertion error if no non-missing values 
+    @test_throws AssertionError initializemice(1, [ missing ])
+    # dimension mismatch error if wrong number of values passed 
+    @test_throws DimensionMismatch MiceValue(5, [ 1, 2, 1 ]) 
+end  
+@testset "Convert missing values in a DataFrame into `Mice` values" begin
+    @testset "DataFrame with no missing values" begin
+        df = DataFrame(; a=[ 1, 2 ], b=[ 3.0, 4.0 ])
+        dft = deepcopy(df)
+        initializemice!(5, dft)
+        @test dft == df
+    end
+    @testset "DataFrame with missing values in first column, column names" begin
+        df = DataFrame(; a=[ 1, missing, 1 ], b=[ 3.0, 4.0, 5.0 ])
+        dft = deepcopy(df)
+        initializemice!(5, dft)
+        # convert missing values in first column
+        @test dft == DataFrame(; a=[ 1, MiceValue(5, ones(Int, 5)), 1 ], b=[ 3.0, 4.0, 5.0 ])
+        # do nothing when given name of column with no missing values 
+        dft2 = deepcopy(df)
+        initializemice!(5, dft2, "b")
+        # need to subdivide test as missing == missing -> missing 
+        @test dft2.b == df.b
+        @test dft2.a[1] == df.a[1]
+        @test ismissing(dft2.a[2])
+        @test dft2.a[3] == df.a[3]
+        # modify first column when given its name
+        dft3 = deepcopy(df)
+        initializemice!(5, dft3, "a")
+        @test dft3 == dft
+        # modify first column when given both column names
+        dft4 = deepcopy(df)
+        initializemice!(5, dft4, [ "a", "b" ])
+        @test dft4 == dft
+    end
+    @testset "DataFrame with missing values in first column, column symbols" begin
+        df = DataFrame(; a=[ 1, missing, 1 ], b=[ 3.0, 4.0, 5.0 ])
+        dft = deepcopy(df)
+        initializemice!(5, dft)
+        # do nothing when given name of column with no missing values 
+        dft2 = deepcopy(df)
+        initializemice!(5, dft2, :b)
+        # need to subdivide test as missing == missing -> missing 
+        @test dft2.b == df.b
+        @test dft2.a[1] == df.a[1]
+        @test ismissing(dft2.a[2])
+        @test dft2.a[3] == df.a[3]
+        # modify first column when given its name
+        dft3 = deepcopy(df)
+        initializemice!(5, dft3, :a)
+        @test dft3 == dft
+        # modify first column when given both column names
+        dft4 = deepcopy(df)
+        initializemice!(5, dft4, [ :a, :b ])
+        @test dft4 == dft
+    end
+    @testset "DataFrame with missing values in first column, column indexes" begin
+        df = DataFrame(; a=[ 1, missing, 1 ], b=[ 3.0, 4.0, 5.0 ])
+        dft = deepcopy(df)
+        initializemice!(5, dft)
+        # do nothing when given index of column with no missing values 
+        dft2 = deepcopy(df)
+        initializemice!(5, dft2, 2)
+        # need to subdivide test as missing == missing -> missing 
+        @test dft2.b == df.b
+        @test dft2.a[1] == df.a[1]
+        @test ismissing(dft2.a[2])
+        @test dft2.a[3] == df.a[3]
+        # modify first column when given its index
+        dft3 = deepcopy(df)
+        initializemice!(5, dft3, 1)
+        @test dft3 == dft
+        # modify first column when given both column indexes
+        dft4 = deepcopy(df)
+        initializemice!(5, dft4, 1:2)
+        @test dft4 == dft
+    end
+    @testset "DataFrame with no missing values and rng" begin
+        df = DataFrame(; a=[ 1, 2 ], b=[ 3.0, 4.0 ])
+        dft = deepcopy(df)
+        rng = StableRNG(1)
+        initializemice!(rng, 5, dft)
+        @test dft == df
+    end
+    @testset "DataFrame with missing values in first column, with rng" begin
+        df = DataFrame(; a=[ 1, missing, 1 ], b=[ 3.0, 4.0, 5.0 ])
+        dft = deepcopy(df)
+        rng = StableRNG(1)
+        initializemice!(rng, 5, dft)
+        # convert missing values in first column
+        @test dft == DataFrame(; a=[ 1, MiceValue(5, ones(Int, 5)), 1 ], b=[ 3.0, 4.0, 5.0 ]) 
+        # accept rng and String
+        dft2 = deepcopy(df)
+        initializemice!(rng, 5, dft2, "a")
+        @test dft2 == dft
+        # accept rng and vector of Strings
+        dft3 = deepcopy(df)
+        initializemice!(rng, 5, dft3, [ "a", "b" ])
+        @test dft3 == dft
+        # accept rng and Symbol
+        dft4 = deepcopy(df)
+        initializemice!(rng, 5, dft4, :a)
+        @test dft4 == dft
+        # accept rng and vector of Symbols
+        dft5 = deepcopy(df)
+        initializemice!(rng, 5, dft5, [ :a, :b ])
+        @test dft5 == dft
+        # accept rng and Index
+        dft6 = deepcopy(df)
+        initializemice!(rng, 5, dft6, 1)
+        @test dft6 == dft
+        # accept rng and vector of Indexes
+        dft7 = deepcopy(df)
+        initializemice!(rng, 5, dft7, 1:2)
+        @test dft7 == dft
+    end
+    @testset "Same rng gives same results" begin
+        @testset for i ∈ [ "a", [ "a", "b" ], :a, [ :a, :b ], 1, 1:2 ]
+            df = DataFrame(; a=[ 1, missing, 2 ], b=[ 3.0, 4.0, 5.0 ])
+            dft_a = deepcopy(df)
+            rng_a = StableRNG(1)
+            initializemice!(rng_a, 5, dft_a, i)
+            dft_b = deepcopy(df)
+            rng_b = StableRNG(1)
+            initializemice!(rng_b, 5, dft_b, i) 
+            @test dft_a == dft_b
+        end
+    end
+    @testset "Different rng gives different results" begin
+        @testset for i ∈ [ "a", [ "a", "b" ], :a, [ :a, :b ], 1, 1:2 ]
+            df = DataFrame(; a=[ 1, missing, 2 ], b=[ 3.0, 4.0, 5.0 ])
+            dft_a = deepcopy(df)
+            rng_a = StableRNG(1)
+            initializemice!(rng_a, 5, dft_a, i)
+            dft_b = deepcopy(df)
+            rng_b = StableRNG(2)
+            initializemice!(rng_b, 5, dft_b, i) 
+            @test dft_a != dft_b
+        end
+    end
+end  
+@testset "Manipulate MiceValues" begin
+    v = [ 1.0, MiceValue(5, [ 1.0, 2.0, 3.0, 4.0, 5.0 ]), 5.0 ]
+    @test micevalues(v, 1) == [ 1.0, 1.0, 5.0 ]
+    @test micevalues(v, 2) == [ 1.0, 2.0, 5.0 ]
+    df = DataFrame( ; 
+        a=[ 1.0, MiceValue(5, [ 1.0, 2.0, 3.0, 4.0, 5.0 ]), 2.0 ], b=[ 3.0, 4.0, 5.0 ]
+    )
+    @test micevalues(df, 1) == [
+        1.0  3.0
+        1.0  4.0
+        2.0  5.0
+    ]
+    @test micevalues(df, 2) == [
+        1.0  3.0
+        2.0  4.0
+        2.0  5.0
+    ]
+    @test micevalues(df, 1, 3) == [
+        1.0  
+        3.0  
+        2.0  
+    ]
+    @test micevalues(df, "a", 4) == [
+        1.0  
+        4.0  
+        2.0  
+    ]
+    @test micevalues(df, :a, 5) == [
+        1.0  
+        5.0  
+        2.0  
+    ]
+    @test micevalues(df, [ :a, :b ], 1) == [
+        1.0  3.0
+        1.0  4.0
+        2.0  5.0
+    ]
+    @test micevalues(df, 1:2, 2) == [
+        1.0  3.0
+        2.0  4.0
+        2.0  5.0
+    ]
+end
+@testset "Arithmetic on MiceValues" begin
+    @test MiceValue(5, [ 0, 1, 2, 3, 4 ]) + 1 == MiceValue(5, [ 1, 2, 3, 4, 5 ])
+    @test 1 + MiceValue(5, [ 0, 1, 2, 3, 4 ]) == MiceValue(5, [ 1, 2, 3, 4, 5 ])
+    s = MiceValue(5, [ 1, 2, 1, 2, 1 ]) + MiceValue(5, [ 0, 1, 2, 3, 4 ])
+    @test s == MiceValue(5, [ 1, 3, 3, 5, 5 ])
+    @test_throws DimensionMismatch MiceValue(3, [ 1, 2, 1 ]) + MiceValue(4, [ 0, 1, 2, 3 ])
+    @test MiceValue(5, [ 1, 2, 3, 4, 5 ]) - 1 == MiceValue(5, [ 0, 1, 2, 3, 4 ])
+    @test 1 - MiceValue(5, [ 0, 1, 2, 3, 4 ]) == MiceValue(5, [ 1, 0, -1, -2, -3 ])
+    s = MiceValue(5, [ 1, 3, 3, 5, 5 ]) - MiceValue(5, [ 0, 1, 2, 3, 4 ])
+    @test MiceValue(5, [ 0, 1, 2, 3, 4 ]) * 2 == MiceValue(5, [ 0, 2, 4, 6, 8 ])
+    @test MiceValue(5, [ 0, 1, 2, 3, 4 ]) / 2 == MiceValue(5, [ 0.0, 0.5, 1.0, 1.5, 2.0 ])
+    @test MiceValue(5, [ 0, 1, 2, 3, 4 ]) ^ 2 == MiceValue(5, [ 0, 1, 4, 9, 16 ])
+    @test s == MiceValue(5, [ 1, 2, 1, 2, 1 ])
+    s2 = exp(MiceValue(5, [ 0, 1, 2, 3, 4 ]))
+    @test s2 == MiceValue(5, [ exp(0), exp(1), exp(2), exp(3), exp(4) ])
+    @test log(MiceValue(2, [ 1, 2 ])) == MiceValue(2, [ log(1), log(2) ])
+    # next three tests use \approx as there were rounding errors. Also tests `isapprox`
+    @test sin(MiceValue(2, [ π, 2 ])) ≈ MiceValue(2, [ sin(π), sin(2) ])
+    @test cos(MiceValue(2, [ π, 2 ])) ≈ MiceValue(2, [ cos(π), cos(2) ])
+    @test tan(MiceValue(2, [ π, 2 ])) ≈ MiceValue(2, [ tan(π), tan(2) ])
+end
+@testset "Update MiceValues, multiple predictive variables" begin
+    df = DataFrame(
+        a = [ 1.0, 2.0, 3.0, 4.0 ], 
+        b = [0.5, 5.0, MiceValue(2, [ 0.5, 5.0 ]), 4.0], 
+        c = [2.0, MiceValue(2, [ 2.0, 2.0 ]), 10.9, 12.0]
+    )
+    dfa = DataFrame(
+        a = [ 1.0, 2.0, 3.0, 4.0 ], 
+        b = [0.5, 5.0, MiceValue(2, [ 0.9859979256186087, 3.441250555637871 ]), 4.0], 
+        c = [2.0, MiceValue(2, [ 2.0, 2.0 ]), 10.9, 12.0]
+    )
+    df2 = deepcopy(df)
+    updatemicevalues!(df2, :b, [ :a, :c ], 2)
+    @test df2 == dfa    
+    df3 = deepcopy(df)
+    updatemicevalues!(df3, "b", [ "a", "c" ], 2)
+    @test df3 == dfa
+    df4 = deepcopy(df)
+    updatemicevalues!(df4, 2, [ 1, 3 ], 2)
+    @test df4 == dfa
+end
+@testset "Update MiceValues, one predictive variable" begin
+    df = DataFrame(
+        a = [ 1.0, 2.0, 3.0, 4.0 ], 
+        b = [0.5, 5.0, MiceValue(2, [ 0.5, 5.0 ]), 4.0], 
+        c = [2.0, MiceValue(2, [ 2.0, 2.0 ]), 10.9, 12.0]
+    )
+    dfa = DataFrame(
+        a = [ 1.0, 2.0, 3.0, 4.0 ], 
+        b = [0.5, 5.0, MiceValue(2, [ 0.9859979256186087, 3.441250555637871 ]), 4.0], 
+        c = [2.0, MiceValue(2, [ 2.0, 2.0 ]), 10.9, 12.0]
+    )
+    df2 = deepcopy(df)
+    updatemicevalues!(df2, :b, :a, 2)
+    @test df2 == dfa    
+    df3 = deepcopy(df)
+    updatemicevalues!(df3, "b", [ "a", "c" ], 2)
+    @test df3 == dfa
+    df4 = deepcopy(df)
+    updatemicevalues!(df4, 2, [ 1, 3 ], 2)
+    @test df4 == dfa
+end
+end  # @testset "SimpleMice.jl"
